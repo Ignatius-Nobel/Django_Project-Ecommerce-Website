a99717278265466ffc33a10984ec35710b3661cf
Ignatius Nobel Chittilappilly <csnobel2001@gmail.com>
2024-07-29 14:13:59 +0530

    completed login and cart pages along with product detail of each product

--- brocart/products/urls.py
+++ brocart/products/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('products_list', views.list_products, name='list_product'),
+    path('products_details', views.detail_product, name='detail_product'),
 ]
