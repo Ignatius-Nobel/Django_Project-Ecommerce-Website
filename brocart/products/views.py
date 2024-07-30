4a8f92d4c8d679bb59698897591f5b7c577f7045
Ignatius Nobel Chittilappilly <csnobel2001@gmail.com>
2024-07-30 18:40:39 +0530

    added pagination to the product list

--- brocart/products/views.py
+++ brocart/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -7,7 +8,14 @@ def list_products(request):
     """_summary_
     returns product list page
     """
+    page = 1
+    if request.GET:
+        page=request.GET.get('page',1)
     product_list = Product.objects.all()
+    # number of products to be displayed in a page
+    product_paginator = Paginator(product_list,2) 
+    # get the page user clicks
+    product_list = product_paginator.get_page(page)
     context = {'products' : product_list}
     return render(request,'products.html',context)
 def detail_product(request):
