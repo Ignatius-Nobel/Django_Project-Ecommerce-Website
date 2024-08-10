50701f802c772de2f4e767105ac86c180577eac5
Ignatius Nobel Chittilappilly <ignatius@americaninference.com>
2024-08-10 16:48:43 +0530

    order checkout

--- brocart/customers/models.py
+++ brocart/customers/models.py
@@ -14,5 +14,5 @@ class Customer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self) -> str:
-        return self.name
+        return self.user.username
 
--- brocart/orders/views.py
+++ brocart/orders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from . models import Order,OrderedItem
 from products.models import Product
+from django.contrib import messages
 
 # Create your views here.
 def show_cart(request):
@@ -35,3 +36,34 @@ def add_to_cart(request):
             ordered_item.quantity+=quantity
             ordered_item.save()
     return redirect('cart')
+
+def remove_item_from_cart(request,pk):
+    item = OrderedItem.objects.get(pk=pk)
+    if item:
+        item.delete()
+    return redirect('cart')
+
+def checkout_cart(request):
+    if request.POST:
+        try:
+            user = request.user
+            customer = user.customer_profile
+            total = float(request.POST.get('total'))
+            order_obj= Order.objects.get(
+                owner = customer,
+                order_status = Order.CART_STAGE
+            )
+            if order_obj:
+                order_obj.order_status = Order.ORDER_CONFIRMED
+                order_obj.total_price = total
+                order_obj.save()
+                status_message = "Your order is processed. Your items will be delivered soon!"
+                messages.success(request,status_message)
+            else:
+                status_message = "Unable to process. No items in cart!!!"
+                messages.error(request,status_message)
+        except Exception as e:
+            status_message = "Something went wrong!"
+            messages.error(request,status_message)
+    return redirect('cart')
+
